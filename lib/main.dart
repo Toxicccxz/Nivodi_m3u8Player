76d747d18b325bf147a3b0cf46145be9bd835d56
Xavier Cui <toxicstrikercuixinzhou@gmail.com>
2026-02-20 16:19:57 -0500

    switch back to niTV

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _WebViewAppState extends State<WebViewApp> {
     super.initState();
     controller = WebViewController()
       ..loadRequest(
-        Uri.parse('https://missav.ws/dm223/en'),
+        Uri.parse('https://www.nivod.cc/'),
       );
   }
 
